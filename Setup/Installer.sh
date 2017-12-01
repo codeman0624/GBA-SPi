9e6a87c6078e1a175ad73a87ff44eaf807f2b7cc
codeman0624 <31072827+codeman0624@users.noreply.github.com>
2017-12-01 13:03:16 -0500

    Update Installer.sh

--- Setup/Installer.sh
+++ Setup/Installer.sh
@@ -1,1 +1,11 @@
 
+
+    cd
+    curl -O https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/retrogame.sh
+    sudo bash retrogame.sh -2
+    
+    #move the proper config file for button setups
+    mv SPi_retrogame.cfg /boot/retrogame.cfg  
+    #move config.txt?
+    
+    #
