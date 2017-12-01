6333fec464914283310aa42109a4cec19e0aadd1
codeman0624 <31072827+codeman0624@users.noreply.github.com>
2017-12-01 13:05:34 -0500

    Update Installer.sh

--- Setup/Installer.sh
+++ Setup/Installer.sh
@@ -1,11 +1,22 @@
+#!/bin/bash
 
+cd ~
+sudo apt-get update
+sudo apt-get install python-serial -y
+sudo apt-get install libpng12-dev -y
+sudo apt-get install python-gpiozero -y
+sudo apt-get install python-pkg-resources python3-pkg-resources -y
+cd ~
+sudo chmod 755 /home/pi/GBA-SPi/BatteryMonitor/Pngview/pngview
+sudo systemctl disable hciuart
 
-    cd
-    curl -O https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/retrogame.sh
-    sudo bash retrogame.sh -2
+cd
+curl -O https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/retrogame.sh
+sudo bash retrogame.sh -2
+
+#move the proper config file for button setups
+mv SPi_retrogame.cfg /boot/retrogame.cfg  
+#move config.txt?
     
-    #move the proper config file for button setups
-    mv SPi_retrogame.cfg /boot/retrogame.cfg  
-    #move config.txt?
-    
-    #
+#move rc.local
+mv SPi_rc.local /etc/rc.local
